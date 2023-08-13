1f04cae159168a29436301be73e211b918b6e40d
Jevgenij Ivanov <ladnopokaa@gmail.com>
2023-08-13 20:22:54 +0100

    Added some code in Systems directory, but most of the work was done in unity engine with the Animator component.

--- Assets/_Scripts/Units/Player/CharacterMovement.cs
+++ Assets/_Scripts/Units/Player/CharacterMovement.cs
@@ -214,25 +214,25 @@ public class CharacterMovement : MonoBehaviour
                 bool isMoving = false;
                 float moveX = 0, moveY = 0;
 
-                if (Input.GetKey(KeyCode.W))
+                if (Input.GetKey(KeyCode.UpArrow))
                 {
                     moveY = 1;
                     isMoving = true;
                 }
 
-                if (Input.GetKey(KeyCode.A))
+                if (Input.GetKey(KeyCode.LeftArrow))
                 {
                     moveX = -1;
                     isMoving = true;
                 }
 
-                if (Input.GetKey(KeyCode.S))
+                if (Input.GetKey(KeyCode.DownArrow))
                 {
                     moveY = -1;
                     isMoving = true;
                 }
 
-                if (Input.GetKey(KeyCode.D))
+                if (Input.GetKey(KeyCode.RightArrow))
                 {
                     moveX = 1;
                     isMoving = true;
